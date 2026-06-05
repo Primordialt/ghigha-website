0d9d906c5b1aecde274f5c268ecc5974d7208ac1
Primordialt <113912642+Primordialt@users.noreply.github.com>
2026-06-05 15:10:02 +0100

    Apollo

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,16 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+o.onload=function(){window.trackingFunctions.onLoad({appId:"6a22c568dca003000c25469b"})},
+document.head.appendChild(o)}initApollo();`,
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col">
         <JsonLd data={getGlobalSchemas()} />
         {children}
